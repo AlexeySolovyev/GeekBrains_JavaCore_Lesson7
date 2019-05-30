918603cd1e1181ed2e36f466a54225a44009065d
Alexey Solovyev <solovevalex@gmail.com>
2019-05-30 17:52:54 +0300

    Initial commit

---
+++ src/Client/MessageReciever.java
@@ -0,0 +1,5 @@
+package Client;
+
+public interface MessageReciever {
+    void submitMessage(TextMessage message);
+}
---
+++ src/Server/ClientHandler.java
@@ -0,0 +1,57 @@
+package Server;
+
+import Client.TextMessage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static Client.MessagePatterns.MESSAGE_SEND_PATTERN;
+import static Client.MessagePatterns.parseTextMessage;
+
+public class ClientHandler {
+
+    private final String login;
+    private final Socket socket;
+    private final DataInputStream inp;
+    private final DataOutputStream out;
+    private final Thread handleThread;
+    private ChatServer chatServer;
+
+    public ClientHandler(String login, Socket socket, ChatServer chatServer) throws IOException {
+        this.login = login;
+        this.socket = socket;
+        this.inp = new DataInputStream(socket.getInputStream());
+        this.out = new DataOutputStream(socket.getOutputStream());
+        this.chatServer = chatServer;
+
+
+        this.handleThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        String msg = inp.readUTF();
+                        System.out.printf("Message from user %s: %s%n", login, msg);
+                        TextMessage textMessage = parseTextMessage(msg);
+                        chatServer.sendMessage(textMessage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+                }
+            }
+        });
+        this.chatServer = chatServer;
+        this.handleThread.start();
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void sendMessage(String userFrom, String userTo, String msg) throws IOException {
+        out.writeUTF(String.format(MESSAGE_SEND_PATTERN, userFrom, userTo, msg));
+    }
+}
